322accc78639ae78ede3ad7b884384147b85d706
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-09-30 12:39:23 -0500

    terminando mini practica con MUI

--- react-With-MUI/src/Components/Dashboard/DashBoard.jsx
+++ react-With-MUI/src/Components/Dashboard/DashBoard.jsx
@@ -1,12 +1,38 @@
 // import { useState } from 'react'
+import { useState, Fragment, useEffect} from 'react';
 import './stylesDashBoard.css'
 
 const DashBoard = () => {
   // const [count, setCount] = useState(0)
 
-  return (
-    <h1> DashBoard </h1>
-  )
-}
+  // const [screenName, setScreenNam] = useState('Hola DashBoard');
+  // const [textoDelAmor, settextoDelAmor] = useState('Hola mi perrita hermosa');
+  const [input, setInput] = useState('')
+
+  const [showPassword, setShowPassword] = useState(false);
+  
+
+  const cambioEnInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const mostrarContrasena = () => {setShowPassword(!showPassword)}  
+
+useEffect(() => {
+  console.log(input);
+}, [input]) 
+
+const alertContrasena = () => {setShowPassword (alert(input))}
+return (
+    <Fragment>
+      <input
+      type={showPassword ? "text": "password" } onChange={cambioEnInput} value={input}>
+      </input>
+      <button onClick={mostrarContrasena } >👀</button>
+      <button onClick={alertContrasena} >👀</button>
+
+    </Fragment>
+  );
+};
 
 export {DashBoard};
--- react-With-MUI/src/Components/Login/Login.jsx
+++ react-With-MUI/src/Components/Login/Login.jsx
@@ -1,14 +1,45 @@
 // import { useState } from 'react'
-import './stylesLogin.css'
-import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import * as React from 'react';
+import './stylesLogin.css';
 
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import InputLabel from '@mui/material/InputLabel';
 
 const Login = () => {
   // const [count, setCount] = useState(0)
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+
 
   return (
     <Box
@@ -21,7 +52,26 @@ const Login = () => {
   >
     <Stack sácomg={8} direction="column">
       <TextField label="Standard" variant="standard" />
-      <TextField label="Password" variant="standard" type="password"/>
+
+      <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+
       <Button variant="contained">Contained</Button>
     </Stack>
 
@@ -29,4 +79,5 @@ const Login = () => {
   )
 }
 
-export {Login};
+export { Login };
+
